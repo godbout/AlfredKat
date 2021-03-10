ac7108861bae464522c6f6e8f57b4db0eba8fd5b
Guill Lo <guill.bout@gmail.com>
2021-03-11 01:02:31 +0800

    add AlfredKatCoreTests module

--- Tests/AlfredKatTests/XCTestManifests.swift
+++ Tests/AlfredKatTests/XCTestManifests.swift
@@ -1,9 +1,9 @@
 import XCTest
 
 #if !canImport(ObjectiveC)
-public func allTests() -> [XCTestCaseEntry] {
-    return [
-        testCase(AlfredKatTests.allTests),
-    ]
-}
+    public func allTests() -> [XCTestCaseEntry] {
+        [
+            testCase(AlfredKatTests.allTests),
+        ]
+    }
 #endif
