b5cabaca69bb23c026d10fe85ade1710d4247361
Guill Lo <guill.bout@gmail.com>
2021-03-12 00:03:59 +0800

    empty all code for now to prepare for implementation

--- Sources/AlfredKat/main.swift
+++ Sources/AlfredKat/main.swift
@@ -1,8 +1,1 @@
-import Foundation
-import AlfredKatCore
 
-if Workflow.next() == "do" {
-    print(Workflow.notify(result: Workflow.do()))
-} else {
-    print(Workflow.menu())
-}
--- Tests/AlfredKatCoreTests/WorkflowTests.swift
+++ Tests/AlfredKatCoreTests/WorkflowTests.swift
@@ -1,24 +1,4 @@
-import XCTest
 @testable import AlfredKatCore
+import XCTest
 
-class WorkflowTests: XCTestCase {
-    private let process = Process()
-    
-    override func setUp() {
-        process.environment = ["action": "LOOOOOOOOLLLL"]
-        
-        super.setUp()
-    }
-}
-
-extension WorkflowTests {
-    func test_that_calling_a_workflow_action_that_does_not_exist_returns_false() {
-        XCTAssertFalse(Workflow.do())
-    }
-    
-    func test_that_calling_a_workflow_action_that_does_not_exists_sends_a_notification_that_huh_you_wrong() {
-        XCTAssertEqual(
-            "huh.", Workflow.notify()
-        )
-    }
-}
+class WorkflowTests: XCTestCase {}
