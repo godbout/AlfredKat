c9c9d11fadf1c9f20780bd49ff50a4261f4c1016
Guill Lo <guill.bout@gmail.com>
2021-03-21 00:38:12 +0800

    use master branch of AlfredWorkflowScriptFilter

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
             name: "AlfredWorkflowScriptFilter",
 //            path: "~/Development/AlfredWorkflowScriptFilter"
             url: "https://github.com/godbout/AlfredWorkflowScriptFilter",
-            .branch("open-up")
+            .branch("master")
         ),
         .package(
             name: "SwiftSoup",
