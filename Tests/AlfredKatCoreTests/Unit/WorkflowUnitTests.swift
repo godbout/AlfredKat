c887d68f22c6b9bb4983e3469ced3b4fb39fb405
Guill Lo <guill.bout@gmail.com>
2021-03-20 20:21:22 +0800

    refactor tests for readability

--- Tests/AlfredKatCoreTests/Unit/WorkflowUnitTests.swift
+++ Tests/AlfredKatCoreTests/Unit/WorkflowUnitTests.swift
@@ -2,21 +2,17 @@
 import XCTest
 
 class WorkflowUnitTests: XCTestCase {
-    override class func setUp() {
-        super.setUp()
-
-        Self.setEnvironmentVariable(name: "action", value: "LOOOOOLLLLLL")
-    }
-}
-
-extension WorkflowUnitTests {
     func test_that_calling_a_workflow_action_that_does_not_exist_returns_false() {
+        Self.setEnvironmentVariable(name: "action", value: "LOOOOOLLLLLL")
+
         XCTAssertFalse(
             Workflow.do()
         )
     }
 
     func test_thatcalling_a_workflow_action_that_does_not_exists_sends_a_notification_that_huh_you_wrong() {
+        Self.setEnvironmentVariable(name: "action", value: "LOOOOOLLLLLL")
+
         XCTAssertTrue(
             Workflow.notify().contains("huh.")
         )
