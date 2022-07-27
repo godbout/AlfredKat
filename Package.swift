fe68c58039aae86967ac98e7fe7373723d8d628e
G <guill.bout@gmail.com>
2022-07-27 17:18:30 +0800

    use swifto tools 5.6, and push min macOS to Catalina 10.15.7

--- Package.swift
+++ Package.swift
@@ -1,19 +1,17 @@
-// swift-tools-version:5.3
+// swift-tools-version: 5.6
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
 let package = Package(
     name: "AlfredKat",
-    platforms: [.macOS(.v10_15)],
+    platforms: [.macOS("10.15.7")],
     dependencies: [
         .package(
-            name: "AlfredWorkflowScriptFilter",
             url: "https://github.com/godbout/AlfredWorkflowScriptFilter",
             from: "1.0.0"
         ),
         .package(
-            name: "SwiftSoup",
             url: "https://github.com/scinfu/SwiftSoup",
             from: "2.3.2"
         ),
