eebacf9bbd42169a18d776cde5a876db5fb2aed6
Guillaume Leclerc <guill@sleeplessmind.com.mo>
2023-06-25 02:34:33 +0800

    update SwiftSoup dep

--- Package.swift
+++ Package.swift
@@ -13,7 +13,7 @@ let package = Package(
         ),
         .package(
             url: "https://github.com/scinfu/SwiftSoup",
-            from: "2.3.2"
+            from: "2.6.0"
         ),
     ],
     targets: [
